6d2a475ab9917c9ed9e0b25cc70ffb9b1b46e83f
Fabio Valerio <jvalerioa@est.utn.ac.cr>
2018-08-29 02:45:30 -0600

    Some Fixed added to the DataService

--- Exam-WebDesign/src/app/data.service.ts
+++ Exam-WebDesign/src/app/data.service.ts
@@ -3,6 +3,9 @@ import { HttpClient } from '@angular/common/http';
 import { MarketPrice } from './Classes/market-price';
 import { AvgBlockSize } from './Classes/avg-block-size';
 import { NTransactions } from './Classes/n-transactions';
+import { MempoolSize } from './Classes/mempool-size';
+import { Minados } from './Classes/minados';
+import { Companies } from './Classes/companies';
 
 
 @Injectable({
@@ -11,9 +14,13 @@ import { NTransactions } from './Classes/n-transactions';
 export class DataService {
   result: any;
   constructor(private _http: HttpClient) {}
+  //Call each one of the blocks from the api
   MARKET_PRICE= 'https://api.blockchain.info/charts/market-price?timespan=5weeks&rollingAverage=8hours&format=json'
   AVG_BLOCK_SIZE='https://api.blockchain.info/charts/avg-block-size?timespan=5weeks&rollingAverage=8hours&format=json'
   N_TRANSACTIONS='https://api.blockchain.info/charts/n-transactions?timespan=5weeks&rollingAverage=8hours&format=json'
+  MEMPOOL = 'https://api.blockchain.info/charts/mempool-size?timespan=5weeks&rollingAverage=8hours&format=json'
+  MINADOS = 'https://api.blockchain.info/stats'
+  COMPANIES = 'https://api.blockchain.info/pools?timespan=5days'
   getMarketPrice(){
     return this._http.get<MarketPrice>(this.MARKET_PRICE);
   }
@@ -22,5 +29,14 @@ export class DataService {
   }
   getNTransactions(){
     return this._http.get<NTransactions>(this.N_TRANSACTIONS);
+  }
+  getMempool(){
+    return this._http.get<MempoolSize>(this.MEMPOOL);
+  }
+  getMinados(){
+    return this._http.get<Minados>(this.MINADOS);
+  }
+  getCompanies(){
+    return this._http.get<Companies>(this.COMPANIES);
   }
    }
--- Exam-WebDesign/src/app/stats-module/stats-module.module.ts
+++ Exam-WebDesign/src/app/stats-module/stats-module.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StatsComponent } from './stats/stats.component';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [StatsComponent]
+  declarations: [StatsComponent, TableComponent]
 })
 export class StatsModuleModule { }
