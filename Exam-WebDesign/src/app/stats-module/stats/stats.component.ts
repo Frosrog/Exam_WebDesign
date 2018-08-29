664829e437c13da87d3973e1783ea24c9723a6e1
Fabio Valerio <jvalerioa@est.utn.ac.cr>
2018-08-29 02:46:01 -0600

    Some Fixed to the stats component

--- Exam-WebDesign/src/app/stats-module/stats/stats.component.ts
+++ Exam-WebDesign/src/app/stats-module/stats/stats.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../data.service';
 
 @Component({
   selector: 'app-stats',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StatsComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private GetData: DataService ) { }
+  BlocksMined: any;
+  minados: any;
+  MinutesBetweenBlocks: number;
+  Minutes: any;
+  VolumenUSD: any;
+  VolumenUSD2: any;
+  estimated_transaction_volume_usd: any;
+  miners_revenue_usd:any;
+  n_btc_mined: any;
+  market_price_usd: any;
+  
+  
   ngOnInit() {
+    this.GetData.getMinados().subscribe(data => {this.minados = data
+    this.BlocksMined = data['n_blocks_mined'];
+    this.MinutesBetweenBlocks=data['minutes_between_blocks']
+    this.Minutes = (this.MinutesBetweenBlocks.toFixed(2))
+    this.VolumenUSD = (data["trade_volume_usd"].toFixed(2));
+    this.estimated_transaction_volume_usd = (data["estimated_transaction_volume_usd"].toFixed(2));
+    this.miners_revenue_usd = (data["miners_revenue_usd"].toFixed(2));
+    this.n_btc_mined = data["n_btc_mined"];
+    this.market_price_usd = data["market_price_usd"]
+
+  })
+    
   }
 
 }
