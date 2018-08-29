187d7c7f8945e225c56e650cac0a7792ae986db4
Fabio Valerio <jvalerioa@est.utn.ac.cr>
2018-08-29 02:44:05 -0600

    More classes added

---
+++ Exam-WebDesign/src/app/Classes/companies.ts
@@ -0,0 +1,23 @@
+export class Companies {
+    values: [{
+        AntPool: number;
+        BTC: number;
+        DPOOL: number;
+        Bixin: number;
+        Unknown: number;
+        BTCC: number;
+        F2Pool:number;
+        ViaBTC: number;
+        SlushPool: number;
+        KanoPool: number;
+        BTCTOP: number;
+        Poolin: number;
+        BitClubNetwork: number;
+        Bitcoincom: number;
+        COIN: number;
+        BitFury:number;
+        CKPool: number;
+
+
+    }];
+}
---
+++ Exam-WebDesign/src/app/Classes/minados.ts
@@ -0,0 +1,22 @@
+export class Minados {
+    values: [{
+        
+        BlocksMined: number;
+        minutes_between_blocks: number;
+        n_btc_mined: number;
+        market_price_usd: number;
+        trade_volume_btc: number;
+        trade_volume_usd: number;
+        totalbc:number;
+        total_fees_btc: number;
+        n_tx: number;
+        total_btc_sent: number;
+        estimated_transaction_volume_usd: number;
+        miners_revenue_btc: number;
+        difficulty: number;
+        hash_rate: number;
+        miners_revenue_usd: number;
+
+
+    }];
+}
